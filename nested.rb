099772e9c198b5622f6c79c3dc96d6683ea62ec2
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-08 17:18:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,8 +21,7 @@ return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -38,6 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+return programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
