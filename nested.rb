aa83c0e77fe31af2c5df5d9c4f38348a61e33c14
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-08 17:47:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] 
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+return programmer_hash
 
 end
